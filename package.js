68f1f55489592f28f6839ff2fe693b3883e07aa9
Jimmie Butler <jimmiebtlr@gmail.com>
2015-02-10 20:17:34 -0600

    Bump version.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'elevatedevdesign:simple-blog-seo',
   summary: 'Package to add default SEO to Simple-Blog',
-  version: '0.0.3',
+  version: '0.0.4',
   git: 'https://github.com/ElevateDevelopmentAndDesign/meteor-simple-blog-seo'
 });
 
