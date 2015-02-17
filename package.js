ac6eede4ba041d9037707d6649e65ae03a90f77d
Jimmie Butler <jimmiebtlr@gmail.com>
2015-02-17 16:28:02 -0600

    Update seo blog index setting.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'elevatedevdesign:simple-blog-seo',
   summary: 'Package to add default SEO to Simple-Blog',
-  version: '0.0.7',
+  version: '0.0.8',
   git: 'https://github.com/ElevateDevelopmentAndDesign/meteor-simple-blog-seo'
 });
 
